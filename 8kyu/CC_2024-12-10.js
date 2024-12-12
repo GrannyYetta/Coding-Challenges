efbee60c3fca6491ecaf9ee5853028d22a3a3e82
GrannyYetta <you@example.com>
2024-12-12 09:54:33 -0600

    minor changes

---
+++ 8kyu/CC_2024-12-10.js
@@ -0,0 +1,32 @@
+/*
+
+Description:
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+a * b
+}
+
+
+*/
+
+function multiply(a, b) {
+	return a * b;
+}
+
+/* 
+
+Description:
+
+Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
+   
+*/
+
+function evenOrOdd(number) {
+	if (number % 2 === 0) {
+		return "Even";
+	} else {
+		return "Odd";
+	}
+}
