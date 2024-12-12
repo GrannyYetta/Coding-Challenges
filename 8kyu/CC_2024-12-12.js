27eabda19b9fc63df0db727f753b29ba9d5a1567
GrannyYetta <you@example.com>
2024-12-12 10:38:41 -0600

    count by x

--- 8kyu/CC_2024-12-12.js
+++ 8kyu/CC_2024-12-12.js
@@ -12,6 +12,27 @@ You get given the time in hours and you need to return the number of litres Nath
 */
 
 function litres(time) {
+	return Math.floor(time * 0.5);
+}
 
-    return Math.floor(time * 0.5);
-  }
+/*
+  
+Description:
+
+Create a function with two arguments that will return an array of the first n multiples of x.
+
+Assume both the given number and the number of times to count will be positive numbers greater than 0.
+
+Return the results as an array or list ( depending on language ).
+
+  */
+
+function countBy(x, n) {
+	let z = [];
+
+	for (let i = 1; i <= n; i++) {
+		z.push(x * i);
+	}
+
+	return z;
+}
