93da62e1f1f555b0513db7b48bc3c0c974a7750a
GrannyYetta <you@example.com>
2024-12-30 15:40:40 +0100

    reduce and sort

---
+++ 8kyu/CC_2024-12-30.js
@@ -0,0 +1,27 @@
+/*
+
+DESCRIPTION:
+
+Given an array of integers your solution should find the smallest integer.
+
+For example:
+
+    Given [34, 15, 88, 2] your solution will return 2
+    Given [34, -345, -1, 100] your solution will return -345
+
+You can assume, for the purpose of this kata, that the supplied array will not be empty.
+
+*/
+
+function findSmallestInt (arr) {
+    const sortedNumbers = arr.sort((a, b) => b-a)
+
+    return sortedNumbers[0]
+}
+
+// ALTERNATIVE:
+
+function findSmallestInt(arr) {
+
+   return Math.min(...arr)
+}
