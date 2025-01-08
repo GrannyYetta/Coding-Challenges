a9b2828995c36dce285edf19dc93db63a1f66a69
GrannyYetta <you@example.com>
2025-01-08 13:29:36 +0100

    solo and sqrt

--- 7kyu/CC_2025-01-07.js
+++ 7kyu/CC_2025-01-07.js
@@ -27,3 +27,16 @@ function removeDuplicateWords(s) {
 function removeDuplicateWords() {
 	return [...new Set(s.split(" "))].join(" ");
 }
+
+
+/*
+
+NOTES:
+
+new Set()
+
+- creates a Set object form the array of words that can only store unique values
+- that means that when you create a new Set from an array, it automatically removes any duplicate values
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+
+*/
---
+++ 8kyu/CC_2025-01-08.js
@@ -0,0 +1,14 @@
+/*
+
+DESCRIPTION:
+
+Complete the solution so that it reverses the string passed into it. 
+
+*/
+
+
+function solution(str){
+    return str.split('').reverse().join('');  
+  }
+
+  
