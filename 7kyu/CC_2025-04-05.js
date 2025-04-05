b3eb2ea7912974be73a2e085ac8691b7ad8a26f4
GrannyYetta <you@example.com>
2025-04-05 21:12:30 +0200

    largest square inside a circle

---
+++ 7kyu/CC_2025-04-05.js
@@ -0,0 +1,15 @@
+/*
+
+DESCRIPTION:
+
+termine the area of the largest square that can fit inside a circle with radius r.
+
+*/
+
+
+function areaLargestSquare(r) {
+const diameter = 2*r
+return diameter*r
+   
+  }
+
