ff81439379be75b3d739b504ae0fd49df6973edc
GrannyYetta <you@example.com>
2025-04-14 15:26:46 +0200

    quick

--- 8kyu/Method Specific/CC_2025-04-14.js
+++ 8kyu/Method Specific/CC_2025-04-14.js
@@ -7,6 +7,19 @@ Write a function which converts the input string to uppercase.
 */
 
 function makeUpperCase(str) {
-    return str.toUpperCase()
-  }
+	return str.toUpperCase();
+}
 
+// ALTERNATIVE SOLUTION
+
+/*
+
+NOTES:
+
+String.prototype.toUpperCase()
+
+
+- returns this string converted to uppercase
+- https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/toUpperCase
+
+*/
